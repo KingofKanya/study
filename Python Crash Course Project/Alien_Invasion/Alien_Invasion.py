b194dc6a588d3ecb6aa0018af8cdae6206812500
红豆刨冰 <114462966+KingofKanya@users.noreply.github.com>
2023-07-10 10:30:50 +0800

    feat:火箭静止在窗口底部中央

--- Python Crash Course Project/Alien_Invasion/Alien_Invasion.py
+++ Python Crash Course Project/Alien_Invasion/Alien_Invasion.py
@@ -1,20 +1,21 @@
 import sys
-from turtle import begin_fill
 
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize game, settings, and screen object.
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
-    # Set the background color.
-    bg_color = (230, 230, 230)
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game.
     while True:
@@ -26,6 +27,7 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
